8e5095fcfc8b46b2bdf035505ba55a2ae95fedc1
Victor Gabriel Pantaleão Santos <53866405+imgabrielvi@users.noreply.github.com>
2019-10-01 15:22:10 -0300

    Update Seguidor de linha PID.ino

--- 2. Seguidor de linha PID/Seguidor de linha PID.ino
+++ 2. Seguidor de linha PID/Seguidor de linha PID.ino
@@ -7,20 +7,23 @@
 #define M1_T  6
 #define M2_T  10
 #define M2_F  11
-#define limite 600
-#define valIn 10
-#define tempoMax 600
+#define limite 730
+#define valIn 100
+#define tempoMax 100
 
-#define Kp 20
-#define Ki 10
-#define Kd 7
+#define Kp 60
+#define Ki 30
+#define Kd 20
 
 unsigned long t1, t2;
 int P, PID, erroAnterior, velMax = valIn + 4*(Kp+Ki+Kd);
 
-byte erro(){
+int erro(){
   int DD = analogRead(dd), D = analogRead(d),
       C = analogRead(c), E = analogRead(e), EE = analogRead(ee);
+  Serial.print(DD); Serial.print(" - "); Serial.print(D); Serial.print(" - ");
+  Serial.print(C); Serial.print(" - "); Serial.print(E); Serial.print(" - ");
+  Serial.print(EE);
   if(DD > limite && D > limite && C > limite && E > limite && EE > limite) return 6;
   if(DD < limite && D < limite && C < limite && E < limite && EE < limite) return 5;
   if(DD < limite && D < limite && C < limite && E < limite && EE > limite) return 4;
@@ -35,7 +38,7 @@ byte erro(){
 }
 
 void calcularPID(){
-  P = erro();
+  P = erro(); Serial.print("     "); Serial.println(P);
   if(P < 5){
     int I = I + P,
         D = erroAnterior - P;
@@ -47,7 +50,7 @@ void calcularPID(){
 void setup() {
   pinMode(M1_F, OUTPUT); pinMode(M1_T, OUTPUT);
   pinMode(M2_F, OUTPUT); pinMode(M2_T,OUTPUT);
-  t1 = millis(); t2 = millis();
+  t1 = millis(); t2 = millis(); Serial.begin(9600);
 }
 
 void loop() {
@@ -61,12 +64,16 @@ void loop() {
 
 void pararMotor(){
   digitalWrite(M1_F, LOW);
-  digitalWrite(M2_F, LOW);
+  digitalWrite(M2_F, LOW); //while(1);
 }
 
 void mover(){
+  /*analogWrite(M1_F, 255);
+  analogWrite(M2_F, 255); //Torque máximo para partida zap
+  */
   int velM1 = valIn + PID,
       velM2 = valIn - PID;
+      
       
   /*********** PWM Arduino ************/ 
     velM1 = map(velM1, 0, velMax, 0, 255);
@@ -75,16 +82,12 @@ void mover(){
     analogWrite(M2_F, velM2);
 
   /********** PWM pelo tempo  *********/
-   /* velM1 = map(velM1, 0, velMax, 0, tempoMax);
+    /*velM1 = map(velM1, 0, velMax, 0, tempoMax);
     velM2 = map(velM2, 0, velMax, 0, tempoMax);
-    if(millis()-t1 < velM1) digitalWrite(M1_F, HIGH);
-      else if(millis()-t1 < tempoMax){
-        digitalWrite(M1_F, LOW);
-        t1 = millis();
-      }
-    if(millis()-t2 < velM2) digitalWrite(M2_F, HIGH);
-      else if(millis()-t2 < tempoMax){
-        digitalWrite(M2_F, LOW);
-        t2 = millis();
-      }*/
+    if(millis()-t1 < velM1) analogWrite(M1_F, 255);
+      else if(millis()-t1 < tempoMax) analogWrite(M1_F, 0);
+    if(millis()-t1>tempoMax) t1 = millis();
+    if(millis()-t2 < velM2) analogWrite(M2_F, 255);
+      else if(millis()-t2 < tempoMax) analogWrite(M2_F, 0);
+    if(millis()-t2>tempoMax) t2 = millis();*/
 }
